4d0a4e9bfcb034bef7bb510b7ac70490d6f8213b
Matt Raible <matt.raible@okta.com>
2022-12-01 00:29:10 -0700

    Enable PWA

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -34,7 +34,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     // jhipster-needle-angular-add-module JHipster will add new module here
     AppRoutingModule,
     // Set this to true to enable service worker (PWA)
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: false }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true }),
     HttpClientModule,
     NgxWebstorageModule.forRoot({ prefix: 'jhi', separator: '-', caseSensitive: true }),
     TranslationModule,
